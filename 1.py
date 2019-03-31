26edc298de32495e4aaf337bd37230be4cd43a45
⋆✰٧⋅ꍏ.ℬ.Ꮇ⋄ChefÃ£o♔✰⋆ <47883842+abmchefao@users.noreply.github.com>
2019-04-01 00:36:48 +0700

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,59 @@
+#!/bin/python
+
+import os, sys
+
+print ("\033[33;1mKalau Gak tau Username sama pass bisa tanya and save aja whatsapp gw")
+
+print ("\033[33;1m-=[0895359088252]=-")
+
+print ("\033[33;1mSEBELUM MASUK KITA LOGIN DULU BOSQUE")
+
+username = 'Xienan' 
+
+password = 'Jessica'
+
+
+def restart():
+
+	ngulang = sys.executable
+
+	os.execl(ngulang, ngulang, *sys.argv)
+
+
+def main():
+
+	uname = raw_input("username : ")
+
+	if uname == username:
+
+		pwd = raw_input("password : ")
+
+
+		if pwd == password:
+
+			sys.exit()
+
+
+		else:
+
+			print "\033[31;1mUsername atau password yang anda masukan salah !!!\033[00m"
+
+
+			restart()
+
+
+	else:
+
+		print "\033[31;1mUsername atau password yang anda masukan salah !!!\033[00m"
+
+
+		restart()
+
+
+try:
+
+	main()
+
+except KeyboardInterrupt:
+
+	os.system('clear')
